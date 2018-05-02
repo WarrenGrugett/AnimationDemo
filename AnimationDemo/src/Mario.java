7c04fd2b99010b16583d8888da4a9aad4de0afcc
wgrugett192 <wgrugett192@PR36.hhs.win.fuhsd.org>
2018-05-02 14:14:30 -0700

    dsfsyS

--- AnimationDemo/src/Mario.java
+++ AnimationDemo/src/Mario.java
@@ -4,53 +4,39 @@ import java.util.*;
 
 import processing.core.PImage;
 
-public class Mario extends Sprite
-{
+public class Mario extends Sprite {
 
-    public static final int MARIO_WIDTH = 40;
-    public static final int MARIO_HEIGHT = 60;
+	public static final int MARIO_WIDTH = 40;
+	public static final int MARIO_HEIGHT = 60;
 
-    public Mario(PImage img, int x, int y)
-    {
-        super(img, x, y, MARIO_WIDTH, MARIO_HEIGHT);
-    }
+	public Mario(PImage img, int x, int y) {
+		super(img, x, y, MARIO_WIDTH, MARIO_HEIGHT);
+	}
 
-    // METHODS
-    public void walk(int dir)
-    {
-<<<<<<< HEAD
-        // WALK!!!
-=======
->>>>>>> branch 'master' of https://github.com/WarrenGrugett/AnimationDemo
-    }
+	// METHODS
+	public void walk(int dir) {
+	}
 
-    public void jump()
-    {
-        moveToLocation(super.x, super.y + MARIO_HEIGHT);
-        moveToLocation(super.x, super.y - MARIO_HEIGHT);
-    }
+	public void jump() {
+		moveToLocation(super.x, super.y + MARIO_HEIGHT);
+		moveToLocation(super.x, super.y - MARIO_HEIGHT);
+	}
 
-    public void act(ArrayList<Shape> obstacles)
-    {
-        // FALL (and stop when a platform is hit)
-        int finalY = 9999999;
-        for (Shape obstacle : obstacles)
-        {
-            if (obstacle.getBounds2D().getWidth() > 0)
-            {
-                if (obstacle.getBounds2D().getX() < x
-                        && obstacle.getBounds2D().getX() + obstacle.getBounds2D().getWidth() > x
-                        && obstacle.getBounds2D().getY() < finalY)
-                {
-                    finalY = (int) (obstacle.getBounds2D().getY() - MARIO_HEIGHT);
-                }
-            }
-            else if (obstacle.getBounds2D().getWidth() < 0)
-            {
+	public void act(ArrayList<Shape> obstacles) {
+		// FALL (and stop when a platform is hit)
+		int finalY = 9999999;
+		for (Shape obstacle : obstacles) {
+			if (obstacle.getBounds2D().getWidth() > 0) {
+				if (obstacle.getBounds2D().getX() < x
+						&& obstacle.getBounds2D().getX() + obstacle.getBounds2D().getWidth() > x
+						&& obstacle.getBounds2D().getY() < finalY) {
+					finalY = (int) (obstacle.getBounds2D().getY() - MARIO_HEIGHT);
+				}
+			} else if (obstacle.getBounds2D().getWidth() < 0) {
 
-            }
-        }
-        moveToLocation(x, finalY);
-    }
+			}
+		}
+		moveToLocation(x, finalY);
+	}
 
 }
