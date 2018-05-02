8894c9f9509e16ebbb3c4dd111526be72b76fbed
wgrugett192 <wgrugett192@PR36.hhs.win.fuhsd.org>
2018-05-02 13:47:17 -0700

    did some more stuff

--- AnimationDemo/src/Mario.java
+++ AnimationDemo/src/Mario.java
@@ -26,10 +26,10 @@ public class Mario extends Sprite {
 
 	public void act(ArrayList<Shape> obstacles) {
 		// FALL (and stop when a platform is hit)
-		int finalY;
+		int finalY = 9999999;
 		for (Shape obstacle : obstacles) {
 			if (obstacle.getBounds2D().getWidth() > 0) {
-				if (obstacle.getBounds2D().getX() < x && obstacle.getBounds2D().getX() + obstacle.getBounds2D().getWidth() > x) {
+				if (obstacle.getBounds2D().getX() < x && obstacle.getBounds2D().getX() + obstacle.getBounds2D().getWidth() > x && obstacle.getBounds2D().getY() < finalY) {
 					finalY = (int) obstacle.getBounds2D().getY();
 				}
 			}
