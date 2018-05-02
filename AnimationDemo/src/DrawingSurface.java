d9a8cfabbccb9d663bb57497cb5aba6a949d7171
snasiriany548 <snasiriany548@PR35.hhs.win.fuhsd.org>
2018-05-02 13:42:27 -0700

    mario

--- AnimationDemo/src/DrawingSurface.java
+++ AnimationDemo/src/DrawingSurface.java
@@ -1,5 +1,4 @@
 
-
 import java.awt.Rectangle;
 import java.awt.Shape;
 import java.awt.event.KeyEvent;
@@ -7,110 +6,116 @@ import java.util.ArrayList;
 import processing.core.PApplet;
 import processing.core.PImage;
 
-public class DrawingSurface extends PApplet {
+public class DrawingSurface extends PApplet
+{
 
-	public static final int DRAWING_WIDTH = 800;
-	public static final int DRAWING_HEIGHT = 600;
+    public static final int DRAWING_WIDTH = 800;
+    public static final int DRAWING_HEIGHT = 600;
 
-	private Rectangle screenRect;
+    private Rectangle screenRect;
 
-	private Mario mario;
-	private ArrayList<Shape> obstacles;
+    private Mario mario;
+    private ArrayList<Shape> obstacles;
 
-	private ArrayList<Integer> keys;
-	
-	private ArrayList<PImage> assets;
+    private ArrayList<Integer> keys;
 
-	public DrawingSurface() {
-		super();
-		assets = new ArrayList<PImage>();
-		keys = new ArrayList<Integer>();
-		screenRect = new Rectangle(0,0,DRAWING_WIDTH,DRAWING_HEIGHT);
-		obstacles = new ArrayList<Shape>();
-		obstacles.add(new Rectangle(200,400,400,50));
-		obstacles.add(new Rectangle(0,250,100,50));
-		obstacles.add(new Rectangle(700,250,100,50));
-		obstacles.add(new Rectangle(375,300,50,100));
-		obstacles.add(new Rectangle(300,250,200,50));
-	}
+    private ArrayList<PImage> assets;
 
+    public DrawingSurface()
+    {
+        super();
+        assets = new ArrayList<PImage>();
+        keys = new ArrayList<Integer>();
+        screenRect = new Rectangle(0, 0, DRAWING_WIDTH, DRAWING_HEIGHT);
+        obstacles = new ArrayList<Shape>();
+        obstacles.add(new Rectangle(200, 400, 400, 50));
+        obstacles.add(new Rectangle(0, 250, 100, 50));
+        obstacles.add(new Rectangle(700, 250, 100, 50));
+        obstacles.add(new Rectangle(375, 300, 50, 100));
+        obstacles.add(new Rectangle(300, 250, 200, 50));
+    }
 
-	public void spawnNewMario() {
-		mario = new Mario(assets.get(0), DRAWING_WIDTH/2-Mario.MARIO_WIDTH/2,50);
-	}
-	
-	public void runMe() {
-		runSketch();
-	}
+    public void spawnNewMario()
+    {
+        mario = new Mario(assets.get(0), DRAWING_WIDTH / 2 - Mario.MARIO_WIDTH / 2, 50);
+    }
 
-	// The statements in the setup() function 
-	// execute once when the program begins
-	public void setup() {
-		//size(0,0,PApplet.P3D);
-		assets.add(loadImage("mario.png"));
-		
-		spawnNewMario();
-	}
+    public void runMe()
+    {
+        runSketch();
+    }
 
-	// The statements in draw() are executed until the 
-	// program is stopped. Each statement is executed in 
-	// sequence and after the last line is read, the first 
-	// line is executed again.
-	public void draw() {
+    // The statements in the setup() function
+    // execute once when the program begins
+    public void setup()
+    {
+        // size(0,0,PApplet.P3D);
+        assets.add(loadImage("mario.png"));
 
-		// drawing stuff
+        spawnNewMario();
+    }
 
-		background(0,255,255);   
+    // The statements in draw() are executed until the
+    // program is stopped. Each statement is executed in
+    // sequence and after the last line is read, the first
+    // line is executed again.
+    public void draw()
+    {
 
-		pushMatrix();
+        // drawing stuff
 
-		float ratioX = (float)width/DRAWING_WIDTH;
-		float ratioY = (float)height/DRAWING_HEIGHT;
+        background(0, 255, 255);
 
-		scale(ratioX, ratioY);
+        pushMatrix();
 
-		fill(100);
-		for (Shape s : obstacles) {
-			if (s instanceof Rectangle) {
-				Rectangle r = (Rectangle)s;
-				rect(r.x,r.y,r.width,r.height);
-			}
-		}
+        float ratioX = (float) width / DRAWING_WIDTH;
+        float ratioY = (float) height / DRAWING_HEIGHT;
 
-		mario.draw(this);
+        scale(ratioX, ratioY);
 
-		popMatrix();
+        fill(100);
+        for (Shape s : obstacles)
+        {
+            if (s instanceof Rectangle)
+            {
+                Rectangle r = (Rectangle) s;
+                rect(r.x, r.y, r.width, r.height);
+            }
+        }
 
+        mario.draw(this);
 
-		// modifying stuff
+        popMatrix();
 
-		if (isPressed(KeyEvent.VK_LEFT))
-			mario.walk(-1);
-		if (isPressed(KeyEvent.VK_RIGHT))
-			mario.walk(1);
-		if (isPressed(KeyEvent.VK_UP))
-			mario.jump();
+        // modifying stuff
 
-		mario.act(obstacles);
+        if (isPressed(KeyEvent.VK_LEFT))
+            mario.walk(-1);
+        if (isPressed(KeyEvent.VK_RIGHT))
+            mario.walk(1);
+        if (isPressed(KeyEvent.VK_UP))
+            mario.jump();
 
-		if (!screenRect.intersects(mario))
-			spawnNewMario();
-	}
+        mario.act(obstacles);
 
+        if (!screenRect.intersects(mario))
+            spawnNewMario();
+    }
 
-	public void keyPressed() {
-		keys.add(keyCode);
-	}
+    public void keyPressed()
+    {
+        keys.add(keyCode);
+    }
 
-	public void keyReleased() {
-		while(keys.contains(keyCode))
-			keys.remove(new Integer(keyCode));
-	}
+    public void keyReleased()
+    {
+        while (keys.contains(keyCode))
+            keys.remove(new Integer(keyCode));
+    }
 
-	public boolean isPressed(Integer code) {
-		return keys.contains(code);
-	}
-
+    public boolean isPressed(Integer code)
+    {
+        return keys.contains(code);
+    }
 
 }
-
--- AnimationDemo/src/edfwefewfweg.java
+++ AnimationDemo/src/edfwefewfweg.java
@@ -1,3 +1,4 @@
-public class edfwefewfweg {
+public class edfwefewfweg
+{
 
 }
