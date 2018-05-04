84c0d89f16ae319bf78a450e774abb0456831cc5
Face733 <warrendg9@gmail.com>
2018-05-03 21:20:35 -0700

    testing still

---
+++ src/Mario.java
@@ -0,0 +1,67 @@
+import java.awt.*;
+import java.awt.geom.*;
+import java.util.*;
+import processing.core.*;
+
+public class Mario extends Sprite {
+	private static final long serialVersionUID = 1L;
+	public static final int MARIO_WIDTH = 40, MARIO_HEIGHT = 60;
+	private final double grav = 2;
+	private boolean jumping = false;
+	private double yVel = 0, xVel = 0;
+
+	public Mario(PImage img, int x, int y) {
+		super(img, x, y, MARIO_WIDTH, MARIO_HEIGHT);
+	}
+
+	public void walk(int dir) {
+		if (Math.abs(xVel) < 6)
+			xVel += dir;
+	}
+
+	public void jump() {
+		if (!jumping) {
+			yVel += 30;
+			jumping = true;
+		}
+	}
+
+	public void act(ArrayList<Shape> obstacles) {
+		yVel -= grav;
+		moveByAmount(0, -yVel);
+		double newY = -1;
+		for (int i = 0; i < obstacles.size(); i++)
+			if (obstacles.get(i).contains(new Point2D.Double(x, y - yVel + MARIO_HEIGHT))
+					|| obstacles.get(i).contains(new Point2D.Double(x + MARIO_WIDTH, y - yVel + MARIO_HEIGHT))
+					|| obstacles.get(i).contains(new Point2D.Double(x, y - yVel))
+					|| obstacles.get(i).contains(new Point2D.Double(x + MARIO_WIDTH, y - yVel))) {
+				if (yVel < 0) {
+					newY = obstacles.get(i).getBounds2D().getY() - MARIO_HEIGHT;
+					yVel = 0;
+					jumping = false;
+				} else if (yVel > 0) {
+					newY = obstacles.get(i).getBounds2D().getY() + obstacles.get(i).getBounds2D().getHeight() + 10;
+					yVel = 0;
+				}
+			}
+		if (newY != -1)
+			moveToLocation(x, newY);
+		if (xVel > 0)
+			xVel -= 0.5;
+		else if (xVel < 0)
+			xVel += 0.5;
+		double newX = -1;
+		for (int i = 0; i < obstacles.size(); i++)
+			if (obstacles.get(i).contains(new Point2D.Double(x + xVel + MARIO_WIDTH, y))) {
+				System.out.println(x + xVel + MARIO_WIDTH + ", " + y);
+				newX = obstacles.get(i).getBounds2D().getX() - MARIO_WIDTH;
+				xVel = 0;
+			} else if (obstacles.get(i).contains(new Point2D.Double(x + xVel, y))) {
+				newX = obstacles.get(i).getBounds2D().getX() + obstacles.get(i).getBounds2D().getWidth();
+				xVel = 0;
+			}
+		moveByAmount(xVel, 0);
+		if (newX != -1)
+			moveToLocation(newX, y);
+	}
+}
---
+++ src/Sprite.java
@@ -0,0 +1,40 @@
+import java.awt.geom.*;
+import processing.core.*;
+
+/*
+ * Represents a moving image.
+ *
+ * by: Shelby
+ */
+
+public class Sprite extends Rectangle2D.Double {
+
+	private static final long serialVersionUID = 1L;
+	private PImage image;
+
+	public Sprite(PImage img, int x, int y, int w, int h) {
+		super(x, y, w, h);
+		image = img;
+	}
+
+	public void moveToLocation(double x, double y) {
+		super.x = x;
+		super.y = y;
+	}
+
+	public void moveByAmount(double x, double y) {
+		super.x += x;
+		super.y += y;
+	}
+
+	public void applyWindowLimits(int windowWidth, int windowHeight) {
+		x = Math.min(x, windowWidth - width);
+		y = Math.min(y, windowHeight - height);
+		x = Math.max(0, x);
+		y = Math.max(0, y);
+	}
+
+	public void draw(PApplet g) {
+		g.image(image, (int) x, (int) y, (int) width, (int) height);
+	}
+}
